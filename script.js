1aa7e7ca8c97bed7d79ba05d2ec50e4c44169615
ChokeySunFlower <chokeyt096@gmail.com>
2025-07-04 14:50:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,7 @@
       }  
 
        <script src="https://unpkg.com/leaflet/dist/leaflet.js">
+  
   <body>const map = L.map('map').setView([28.3949, 84.1240], 5);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; OpenStreetMap contributors'
