f78b8164b9e31446227e319ccdcdc45cf226d638
ChokeySunFlower <chokeyt096@gmail.com>
2025-07-04 13:38:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
- </head>
-  <body>
-    <h1>&lt;h1&gt; Tibetan Dog</h1>
+ </button>
+    <p>And JavaScript adds interactivity. This button shows an alert.</p>
+       <h1>This is a &lt;script&gt; tag example</h1>
+    <p>Click the button to enter your name and see JavaScript in action!</p>
 
-    <p>&lt;p&gt </p>big dogs who are domisticated by people living in Tibet</p>
-
-    <ul>
-      <li>&lt;ul&gt; creates an unordered list:</li>
-      <li>&lt;li&gt; Each &lt;li&gt; is a list item</li>
-    </ul>
-
-    <a href="https://TibetanDog.#001" target="_blank">&lt;a&gt; This is a link tag — it goes to another webpage</a>
-
-    <br><br>
-
-    <img src="one-monastery.jpg" alt="TibetanDogs.html" width="200">
-    <p>&lt;img&gt; displays an image. The <code>src</code> attribute is the image URL.</p>
+    <button onclick="greetUser()">I will say Tashi Delek to your visior</button>
+    <script>
+      // This function runs when the button is clicked
+      function greetUser() {
+        let name = prompt("What's your name? \n ཁྱེད་རང་གི་མིང་ལ་ག་རེ་རེད།");
+        if (name) {
+          alert("Tashi Delek བཀྲ་ཤིས་བདེ་ལེགས།, " + name + "! 👋");
+        } else {
+          alert("You didn't enter a name.");
+        }
+      }
+    </script>
